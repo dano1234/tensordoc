e6982d3a8b1fa7500f8600e94de649584a2f4514
Dano <dan.osullivan@nyu.edu>
2023-06-01 14:32:27 -0400

    hf

--- app.py
+++ app.py
@@ -2,6 +2,20 @@ import os
 import time
 import uuid
 
+
+from huggingface_hub import login
+
+huggingface_key = os.environ.get("huggingface_key")
+print("huggingface_key")
+print(huggingface_key)
+login(token='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')
+
+print("checking CUDA")
+print(f"Is CUDA available: {torch.cuda.is_available()}")
+# True
+print(
+    f"CUDA device: {torch.cuda.get_device_name(torch.cuda.current_device())}")
+
 from fastapi import FastAPI
 from fastapi.exceptions import HTTPException
 from fastapi.responses import FileResponse
