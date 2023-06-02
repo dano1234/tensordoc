e08e4a7b7dfd915a45955361fc32cee8f84c2fc6
Dano <dan.osullivan@nyu.edu>
2023-06-02 11:04:16 -0400

    hh

--- app.py
+++ app.py
@@ -539,8 +539,8 @@ async def create_item(oneImage: OneImage):
 
 ngrok_tunnel = ngrok.connect(8000)
 print('Public URL:', ngrok_tunnel.public_url)
-nest_asyncio.apply()
-uvicorn.run(app, port=8000)
+#nest_asyncio.apply()
+#uvicorn.run(app, port=8000)
 
 
 
